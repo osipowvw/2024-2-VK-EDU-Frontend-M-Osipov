6141ce81afdad7c2123938db22ee0a10f0e3ffb5
osipowvw <osipov.maksim2802@gmail.com>
2024-09-20 21:18:57 +0300

    Modified gitignore and one function

--- dz1/convertBytesToHuman.test.js
+++ dz1/convertBytesToHuman.test.js
@@ -26,6 +26,7 @@ test('Возвращает корректное значение для чисе
   expect(convertBytesToHuman(123123123)).toBe('117.42 MB');
   expect(convertBytesToHuman(1073741824)).toBe('1 GB');
   expect(convertBytesToHuman(1099511627776)).toBe('1 TB');
+  expect(convertBytesToHuman(1536)).toBe('1.5 KB');
 });
 
 test('Пограничные значения', () => {
@@ -35,4 +36,6 @@ test('Пограничные значения', () => {
   expect(convertBytesToHuman(1073741823)).toBe('1 GB');
   expect(convertBytesToHuman(1099511627775)).toBe('1 TB');
   expect(convertBytesToHuman(1099511627777)).toBe('1 TB');
+  expect(convertBytesToHuman(1537)).toBe('1.5 KB');
+  expect(convertBytesToHuman(1535)).toBe('1.5 KB');
 });
